3583d219e22033ccadbfb23bf1a8d7a310b02fdc
RianAFK <rian.colombaro@aluno.senai.br>
2026-03-26 13:49:09 -0300

    feat: Estrutura para as tabelas e migrations

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models.Entities;
+
+namespace ToDoList.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Passo 1 foi criar as entidades (Models/Entities/Usuario.cs)
+        // Passo 2 é criar o DbContext (este arquivo) e registrar as entidades como DbSet
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Tarefa>()
+                .HasOne(t => t.Usuario)
+                .WithMany(u => u.Tarefas)
+                .HasForeignKey(t => t.UsuarioId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
